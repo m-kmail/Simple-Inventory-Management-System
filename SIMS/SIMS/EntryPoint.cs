a57636a02039655841e9ee78e5685b2143cfd244
M_Kmail <kmailmohammad09@gmail.com>
2023-08-23 09:46:11 +0300

    DONE...setting up the project

---
+++ SIMS/SIMS/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIMS
+{
+    public class EntryPoint
+    {
+
+        public static void Main()
+        {
+            SIMS manager = new();
+            manager.Main();
+        }
+    }
+}
